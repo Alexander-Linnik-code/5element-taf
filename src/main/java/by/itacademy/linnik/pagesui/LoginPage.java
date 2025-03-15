e164dacb1b9143630495b67b91949c0cf795ba11
Alex Linnik <SanchezAnd3@gmail.com>
2025-03-15 16:37:56 +0300

    added methods and logging for working with the login page.

--- src/main/java/by/itacademy/linnik/pagesui/LoginPage.java
+++ src/main/java/by/itacademy/linnik/pagesui/LoginPage.java
@@ -1,7 +1,11 @@
 package by.itacademy.linnik.pagesui;
 
 import by.itacademy.linnik.singleoton.Singleton;
+import io.qameta.allure.Step;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
@@ -21,4 +25,84 @@ public class LoginPage {
 
     private final By CONFIRM_PHONE_BUTTON = By.xpath("(//button[@class='btn btn--lg btn--block submit-button'])[1]");
 
+    private static final Logger logger = LogManager.getLogger(LoginPage.class);
+
+    @Step
+    public LoginPage enterPhoneNumber(String phoneNumber) {
+        try {
+            driver.findElement(LINE_FOR_ENTERING_A_PHONE_NUMBER).sendKeys(phoneNumber);
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to enter a phone number but the field was not found.");
+            logger.error("Element to click not found", e);
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to enter text but an error occurred.");
+            logger.error("There was an error entering your phone number");
+            throw e;
+        }
+    }
+
+    @Step
+    public LoginPage clickGetCode() {
+        try {
+            driver.findElement(GET_CODE_BUTTON).click();
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click but element not found.");
+            logger.error("Element to click not found");
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click but an error occurred.");
+            logger.error("An error occurred while attempting to click.");
+            throw e;
+        }
+    }
+
+    @Step
+    public LoginPage enteringCodeFromSms(String code) {
+        try {
+            driver.findElement(CODE_ENTRY_FIELD).sendKeys(code);
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to enter a number from SMS, but the element was not found.");
+            logger.error("Code entry field not found", e);
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to enter a number from SMS, but an error occurred.");
+            logger.error("Error encountered while trying to enter SMS code", e);
+            throw e;
+        }
+    }
+
+    @Step
+    public LoginPage clickConfirmPhoneButton() {
+        try {
+            driver.findElement(CONFIRM_PHONE_BUTTON).click();
+            return this;
+        } catch (NoSuchElementException e) {
+            logger.info("Attempted to click on phone number verification, but the element was not found.");
+            logger.error("Confirm phone button not found", e);
+            throw e;
+        } catch (Exception e) {
+            logger.info("Attempted to click on confirm phone number, but an error occurred.");
+            logger.error("Error encountered while trying to click confirm phone button", e);
+            throw e;
+        }
+    }
+
+    @Step
+    public String gettingErrorText() {
+        try {
+            return driver.findElement(TEXT_IS_NOT_CORRECT_CODE).getText();
+        } catch (NoSuchElementException e) {
+            logger.info("Trying to extract text, but the element was not found.");
+            logger.error("Element not found", e);
+            return "Element not found";
+        } catch (Exception e) {
+            logger.info("Trying to extract text, but an error occurred.");
+            logger.error("Error encountered while trying to get error text", e);
+            return "Error occurred: " + e.getMessage();
+        }
+    }
 }
