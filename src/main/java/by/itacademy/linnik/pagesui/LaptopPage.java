ddb100d2192e42662ce5c466ac82d586c8a0a0ab
Alex Linnik <SanchezAnd3@gmail.com>
2025-03-16 17:33:02 +0300

    added a method for working with the laptop page

--- src/main/java/by/itacademy/linnik/pagesui/LaptopPage.java
+++ src/main/java/by/itacademy/linnik/pagesui/LaptopPage.java
@@ -1,9 +1,11 @@
 package by.itacademy.linnik.pagesui;
 
 import by.itacademy.linnik.singleoton.Singleton;
+import io.qameta.allure.Step;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class LaptopPage {
@@ -16,4 +18,19 @@ public class LaptopPage {
     private static final Logger logger = LogManager.getLogger(LaptopPage.class);
 
     private final By TITLE_LAPTOP = By.xpath("//h1");
+
+    @Step
+    public String  getTextModelLaptop(){
+        try {
+            return driver.findElement(TITLE_LAPTOP).getText();
+        } catch (NoSuchElementException e) {
+            logger.info("Trying to extract text, but the element was not found.");
+            logger.error("Element not found", e);
+            return "Element not found";
+        } catch (Exception e) {
+            logger.info("Trying to extract text, but an error occurred.");
+            logger.error("Error encountered while trying to get error text", e);
+            return "Error occurred: " + e.getMessage();
+        }
+    }
 }
