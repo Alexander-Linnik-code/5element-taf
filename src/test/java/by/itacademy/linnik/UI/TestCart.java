6780e0025322b7ed915c3b2e7ab05f29176ed127
Alex Linnik <SanchezAnd3@gmail.com>
2025-03-16 17:51:01 +0300

    added a test for working with the basket

---
+++ src/test/java/by/itacademy/linnik/UI/TestCart.java
@@ -0,0 +1,25 @@
+package by.itacademy.linnik.UI;
+
+import by.itacademy.linnik.pagesui.HomePage;
+import by.itacademy.linnik.singleoton.Singleton;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCart {
+        @Test
+        @Description("Adding and then removing an item from the shopping cart, checking the title.")
+        public void test1(){
+            String actual = new HomePage()
+                    .open()
+                    .clickSearchAndWriteTextAndClickEnter("Lenovo")
+                    .clickCookie()
+                    .scrollSite()
+                    .clickAddCart()
+                    .clickCart()
+                    .clickRemoveButton()
+                    .getTextCartEmpty();
+            Assertions.assertEquals("В корзине еще нет товаров",actual);
+            Singleton.quit();
+        }
+}
