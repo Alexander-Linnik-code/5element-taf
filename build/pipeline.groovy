bd5339dc9fccfeac04a4bd1b70c98364c0463b0a
Alex Linnik <SanchezAnd3@gmail.com>
2025-06-15 15:09:35 +0300

    add builds

--- build/pipeline.groovy
+++ build/pipeline.groovy
@@ -10,6 +10,11 @@ pipeline {
 
         stage('Build') {
             steps {
+                bat 'javac -d . "%WORKSPACE%\\src\\main\\java\\by\\itacademy\\linnik\\pagesui\\CardPade.java"'
+                bat 'javac -d . "%WORKSPACE%\\src\\main\\java\\by\\itacademy\\linnik\\pagesui\\CatalogPage.java"'
+                bat 'javac -d . "%WORKSPACE%\\src\\main\\java\\by\\itacademy\\linnik\\pagesui\\HomePage.java"'
+                bat 'javac -d . "%WORKSPACE%\\src\\main\\java\\by\\itacademy\\linnik\\pagesui\\LaptopPage.java"'
+                bat 'javac -d . "%WORKSPACE%\\src\\main\\java\\by\\itacademy\\linnik\\pagesui\\LoginPage.java"'
                 bat 'javac -d . "%WORKSPACE%\\src\\test\\java\\by\\itacademy\\linnik\\ui\\TestCart.java"'
             }
         }
